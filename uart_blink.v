5e22351b245dcb8e917219d7032a020f8b211d37
Jacob Killelea <Jkillelea@protonmail.ch>
2019-05-24 15:48:23 -0700

    store uart to ram and then retrieve

--- uart_blink.v
+++ uart_blink.v
@@ -14,14 +14,42 @@ module UART_Blink(
     output RLED4,
 );
 
-    wire       tx_dv;
+    wire       rx_dv;
+    wire [7:0] rx_byte;
+
+    uart_rx #(.UART_BAUD(9600)) uart(
+        ICE_CLK,
+        UART_RX,
+        rx_dv,
+        rx_byte
+    );
+
+    wire [7:0] ram_din;
+    reg  [8:0] ram_write_addr = 0;
+    reg        ram_write_en;
+    reg  [8:0] ram_read_addr  = 0;
+    wire [7:0] ram_dout;
+
+    assign ram_din      = rx_byte;
+    assign ram_write_en = rx_dv;
+
+    ram ram_inst (
+        ram_din,
+        ram_write_addr,
+        ram_write_en,
+        ICE_CLK,
+        ram_read_addr,
+        ram_dout
+    );
+
+    reg        tx_dv;
     wire [7:0] tx_byte;
     wire       tx_data;
     wire       tx_done;
 
-    wire       rx_dv;
-    wire [7:0] rx_byte;
-    wire       is_data;
+    wire rx_ahead;
+    assign rx_ahead = (ram_write_addr > ram_read_addr);
+    assign tx_byte  = ram_dout;
 
     uart_tx2 #(.UART_BAUD(9600)) tx(
         ICE_CLK,
@@ -31,39 +59,29 @@ module UART_Blink(
         tx_done
     );
 
-    uart_rx #(.UART_BAUD(9600)) uart(
-        ICE_CLK,
-        UART_RX,
-        rx_dv,
-        rx_byte
-    );
-
-    always @(rx_dv) begin
-        tx_byte <= rx_byte;
-        tx_dv   <= rx_dv;
+    always @(posedge rx_dv) begin
+        ram_write_addr <= ram_write_addr + 1;
     end
 
-    assign tx_byte  = rx_byte;
-    assign tx_dv    = rx_dv;
+    always @(posedge ICE_CLK) begin
+        if ((ram_write_addr >= ram_read_addr)) begin // write > read -> read++, tx action
+            ram_read_addr <= ram_read_addr + 1;
+            tx_dv <= 1;
+        end else begin                               // write <= read -> nothing
+            // nothing
+            tx_dv <= 0;
+        end
+    end
 
-    assign UART_TX  = tx_data;
-    // initial begin
-    //     tx_byte <= 65;
-    //     tx_dv   <= 1;
-    // end
-    // always begin
-    //     #10000000
-    //     tx_byte <= tx_byte + 1;
-    // end
+    assign UART_TX = tx_data;
+    assign J3_9    = UART_TX;
+    assign J3_10   = tx_dv;
 
-    // assign UART_TX = UART_RX;
-    assign J3_9  = UART_TX;
-    assign J3_10 = UART_RX;
-
-    always RLED1 <= rx_byte[0];
-    always RLED2 <= rx_byte[1];
-    always RLED3 <= rx_byte[2];
-    always RLED4 <= rx_byte[3];
-    always GLED5 <= rx_byte[4];
-
+    // reg [8:0] addr_difference;
+    // assign addr_difference = ram_write_addr - ram_read_addr;
+    // assign RLED1 = addr_difference[0];
+    // assign RLED2 = addr_difference[1];
+    // assign RLED3 = addr_difference[2];
+    // assign RLED4 = addr_difference[3];
+    // assign GLED5 = addr_difference[4];
 endmodule
