cac3c636bdaac3e267c00c56a3331865a92e3240
Jacob Killelea <Jkillelea@protonmail.ch>
2019-11-28 21:32:21 -0800

    testbench update

--- uart_mirror/tb.v
+++ uart_mirror/tb.v
@@ -3,59 +3,12 @@
 
 module tb();
 
-    parameter BITPER = 104_000; // 104 us
-
-    initial begin
-        $dumpfile("test.vcd");
-        $dumpvars(0, top_inst);
-        uart_rx = 1;
-        #10_000; // 0.01 ms
-
-        // clock out 0x0D
-        uart_rx = 0;
-        #BITPER; // 105 us
-        uart_rx = 1;
-        #BITPER; // 105 us
-        uart_rx = 0;
-        #BITPER; // 105 us
-        uart_rx = 1;
-        #BITPER; // 105 us
-        #BITPER; // 105 us
-        uart_rx = 0;
-        #BITPER; // 105 us
-        #BITPER; // 105 us
-        #BITPER; // 105 us
-        #BITPER; // 105 us
-        uart_rx = 1; // stop bit
-        #BITPER; // 105 us
-
-        // send 0x0A
-        uart_rx = 0; // start bit
-        #BITPER; // 105 us
-        uart_rx = 0;
-        #BITPER; // 105 us
-        uart_rx = 1;
-        #BITPER; // 105 us
-        uart_rx = 0;
-        #BITPER; // 105 us
-        uart_rx = 1;
-        #BITPER; // 105 us
-        uart_rx = 0;
-        #BITPER; // 105 us
-        #BITPER; // 105 us
-        #BITPER; // 105 us
-        #BITPER; // 105 us
-        uart_rx = 1; // stop bit
-        #BITPER; // 105 us
-
-        #5_000_000; // 5 ms
-        $finish;
-    end
+    parameter BITPER = 104_000; // 104 us = 9600 bps
 
     reg clk = 0;
     always #42 clk <= !clk;
 
-    reg uart_rx = 1;
+    reg  uart_rx = 1;
     wire uart_tx;
     wire j3_10;
     wire j3_9;
@@ -71,5 +24,76 @@ module tb();
         j3_10, j3_9, j3_8,
         gled5, rled1, rled2, rled3, rled4
     );
+
+    task uart_signal (input [7:0] byte);
+        integer i;
+        begin
+            // Start bit
+            uart_rx = 0;
+            #BITPER;
+            for (i = 0; i < 8; i++) begin
+                uart_rx = byte[i];
+                #BITPER;
+            end
+            // Stop bit
+            uart_rx = 1;
+            #BITPER;
+        end
+    endtask
+
+    initial begin
+        $dumpfile("test.vcd");
+        $dumpvars(0, top_inst);
+        uart_rx = 1;
+        #10_000; // 0.01 ms
+
+        uart_signal(8'h01);
+        uart_signal(8'h02);
+        uart_signal(8'h03);
+        uart_signal(8'h04);
+
+        uart_signal(8'h0D);
+        // // clock out 0x0D
+        // uart_rx = 0; // start bit
+        // #BITPER;
+        // uart_rx = 1;
+        // #BITPER;
+        // uart_rx = 0;
+        // #BITPER;
+        // uart_rx = 1;
+        // #BITPER;
+        // #BITPER;
+        // uart_rx = 0;
+        // #BITPER;
+        // #BITPER;
+        // #BITPER;
+        // #BITPER;
+        // uart_rx = 1; // stop bit
+        // #BITPER;
+
+        uart_signal(8'h0A);
+        // // send 0x0A
+        // uart_rx = 0; // start bit
+        // #BITPER; // 105 us
+        // uart_rx = 0;
+        // #BITPER; // 105 us
+        // uart_rx = 1;
+        // #BITPER; // 105 us
+        // uart_rx = 0;
+        // #BITPER; // 105 us
+        // uart_rx = 1;
+        // #BITPER; // 105 us
+        // uart_rx = 0;
+        // #BITPER; // 105 us
+        // #BITPER; // 105 us
+        // #BITPER; // 105 us
+        // #BITPER; // 105 us
+        // uart_rx = 1; // stop bit
+        // #BITPER; // 105 us
+
+
+        #5_000_000; // 5 ms
+        $finish;
+    end
 
 endmodule
