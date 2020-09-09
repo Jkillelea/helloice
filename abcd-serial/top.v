ed0554b8a78db88aaa44588e77b3e72294009b67
Jacob Killelea <jkillelea@protonmail.ch>
2020-09-08 21:37:26 -0500

    Misc

--- abcd-serial/top.v
+++ abcd-serial/top.v
@@ -42,45 +42,46 @@ module top (
 
     wire [3:0] leds = {RLED1, RLED2, RLED3, RLED4};
     assign leds = tx_byte[6:3];
-    assign GLED5 = !UART_TX;
+    // assign GLED5 = !UART_TX;
+    assign GLED5 = pll_locked;
     assign OUT106 = !UART_TX;
 
 
     reg [6:0] posoff = 0;
     parameter START = "0";
 
-    // // generate ascii sawtooth
-    // always @(posedge tx_done) begin
-    //     // end of line
-    //     if (tx_byte > START + posoff) begin
-    //         tx_byte <= "\n";
-
-    //         // last sawtooth
-    //         if (posoff == "z" - "1") // reset position offset
-    //             posoff <= 0;
-    //         else
-    //             posoff <= posoff + 1;
-    //     end
-    //     else if (tx_byte == "\n") begin // restart at START char
-    //         tx_byte <= START;
-    //     end
-    //     else begin // print next in sequence
-    //         tx_byte <= tx_byte + 1;
-    //     end
-    // end
-
-    reg [31:0] counter = 0;
-    reg [4:0] bitpos = 4'hF;
+    // generate ascii sawtooth
     always @(posedge tx_done) begin
-        bitpos <= bitpos - 1;
-        counter <= counter;
-        if (bitpos == 0) begin
+        // end of line
+        if (tx_byte > START + posoff) begin
             tx_byte <= "\n";
-            counter <= counter + 1;
-        end else begin
-            tx_byte <= counter[bitpos] ? "1" : "0";
+
+            // last sawtooth
+            if (posoff == "z" - "1") // reset position offset
+                posoff <= 0;
+            else
+                posoff <= posoff + 1;
+        end
+        else if (tx_byte == "\n") begin // restart at START char
+            tx_byte <= START;
+        end
+        else begin // print next in sequence
+            tx_byte <= tx_byte + 1;
         end
     end
+
+    // reg [31:0] counter = 0;
+    // reg [4:0] bitpos = 4'hF;
+    // always @(posedge tx_done) begin
+    //     bitpos <= bitpos - 1;
+    //     counter <= counter;
+    //     if (bitpos == 0) begin
+    //         tx_byte <= "\n";
+    //         counter <= counter + 1;
+    //     end else begin
+    //         tx_byte <= counter[bitpos] ? "1" : "0";
+    //     end
+    // end
 
     // print out 0 thru 9
     // reg [3:0] n = 0;
